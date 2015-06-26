3f56c928395a8d8fc6a50b5c531ceefb5abd9cd6
Maya <waverley@waverleys-Mac-mini.local>
2015-06-26 12:21:19 -0700

    builder

---
+++ Autolayout-DZ1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayout-DZ1
+//
+//  Created by Maya on 6/24/15.
+//  Copyright (c) 2015 Maya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Autolayout-DZ1/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Autolayout-DZ1
+//
+//  Created by Maya on 6/24/15.
+//  Copyright (c) 2015 Maya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(NSUInteger)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskAll;
+}
+
+@end
